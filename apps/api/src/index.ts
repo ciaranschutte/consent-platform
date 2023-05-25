1f548bd455ddf2c1e04335cbaf5fbd1f41dc0c3e
Rakesh Mistry <mistryrn@mcmaster.ca>
2023-05-25 14:36:56 -0400

    🗃️ Initial DB and Docker Setup * Add docker-compose.yaml with postgres * Create a dummy migration * Connect Prisma to Express app

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,13 +1,13 @@
 import express from 'express';
-// import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 
-// const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/users', async (req, res) => {
-  // const users = await prisma.user.findMany();
-  res.json({"users": []});
+  const users = await prisma.user.findMany();
+  res.json({"users": [users]});
 });
 
 app.listen(port, () => {
