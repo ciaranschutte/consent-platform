6599a08f6063ede10d5e4149a3c7b0f189a3f74f
Ann Catton <anncatton@gmail.com>
2023-07-26 14:42:20 -0400

    formatting css files, todo for font sizes to rem units

--- apps/ui/tailwind.config.js
+++ apps/ui/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 
-import colors from './src/theme/colors'
+import colors from './src/theme/colors';
 
 module.exports = {
   content: [
@@ -11,23 +11,22 @@ module.exports = {
   theme: {
     colors,
     fontFamily: {
-      sans: ['var(--font-montserrat)']
+      sans: ['var(--font-montserrat)'],
     },
+    // TODO: add these as rem values, will get updated when style guide updated
     fontSize: {
-      xs: ['13px', {lineHeight: '15.85px', fontWeight: '400'}],
-      sm: ['14px', {lineHeight: '16px', fontWeight: '400'}],
-      base: ['16px', {lineHeight: '19.5px', fontWeight: '400'}],
-      lg: ['18px', {lineHeight: '22px', fontWeight: '400'}],
-      xl: ['24px', {lineHeight: '29px', fontWeight: '400'}],
-      '2xl': ['46px', {lineHeight: '56px', fontWeight: '400'}],
+      xs: ['13px', { lineHeight: '15.85px', fontWeight: '400' }],
+      sm: ['14px', { lineHeight: '16px', fontWeight: '400' }],
+      base: ['16px', { lineHeight: '19.5px', fontWeight: '400' }],
+      lg: ['18px', { lineHeight: '22px', fontWeight: '400' }],
+      xl: ['24px', { lineHeight: '29px', fontWeight: '400' }],
+      '2xl': ['46px', { lineHeight: '56px', fontWeight: '400' }],
     },
     fontWeight: {
       normal: '400',
       bold: '600',
     },
-    extend: {
-
-    },
+    extend: {},
   },
   plugins: [],
-}
+};
