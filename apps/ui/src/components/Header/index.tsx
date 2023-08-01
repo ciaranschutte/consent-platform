43956b650f78b882d37b6c430b4888676441041c
anncatton <anncatton@gmail.com>
2023-08-01 16:15:24 -0400

    add todos

--- apps/ui/src/components/Header/index.tsx
+++ apps/ui/src/components/Header/index.tsx
@@ -64,9 +64,12 @@ const Header = async ({ lang }: { lang: ValidLanguage }) => {
             currentLang={{ lang, translated: translate(lang) }}
           />
         </div>
+        {/* TODO: implement real help button, ticket TBD */}
         <div className={styles['hide-on-mobile']}>
           <HelpButton />
         </div>
+        {/* TODO: implement mobile language toggle inside user menu in separate PR for https://github.com/OHCRN/consent-platform/issues/16 */}
+        {/* TODO: implement user menu, ticket TBD */}
         <div className={styles['user-menu']}>
           <div>Hello, User</div>
         </div>
